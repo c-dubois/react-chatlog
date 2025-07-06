ff18afef7b6dcb7d5d196aa833bee87e4cf5c674
Camille <camilledubois1995@gmail.com>
2025-07-07 01:36:33 +0200

    update App with ChatEntry component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import ChatEntry from './components/ChatEntry';
+import messages from './data/messages.json';
 import './App.css';
 
 const App = () => {
+  const firstMessage = messages[0];
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Camille's Chatroom</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={firstMessage.sender}
+          body={firstMessage.body}
+          timeStamp={firstMessage.timeStamp}
+        />
       </main>
     </div>
   );
