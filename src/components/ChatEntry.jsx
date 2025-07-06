6f82b310e7ea872a0e7c63edb37d69415601ce32
Camille <camilledubois1995@gmail.com>
2025-07-07 01:29:53 +0200

    update ChatEntry to accept props

--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,12 +1,16 @@
+import PropTypes from 'prop-types';
+
 import './ChatEntry.css';
 
-const ChatEntry = () => {
+const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time">
+          <Timestamp timeStamp={props.timeStamp} />
+        </p>
         <button className="like">🤍</button>
       </section>
     </div>
